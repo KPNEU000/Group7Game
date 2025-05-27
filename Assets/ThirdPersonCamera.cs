0f5034a704ce3f03cf05de4ea4fa49208d9217e4
KPNEU000 <pauwels.k@northeastern.edu>
2025-05-27 19:33:16 -0400

    Camera Changing Level 1

---
+++ Assets/ThirdPersonCamera.cs
@@ -0,0 +1,13 @@
+using Unity.Cinemachine;
+using UnityEngine;
+
+public class ThirdPersonCamera : MonoBehaviour
+{
+    //public Camera thirdPersonCamera;
+    public GameObject firstPersonCamera;
+    public void UpdateCameraPosition(GameObject player, GameObject NPC)
+    {
+        transform.position = Vector3.Lerp(player.transform.position, NPC.transform.position, 0.5f);
+        firstPersonCamera.SetActive(false);
+    }
+}
